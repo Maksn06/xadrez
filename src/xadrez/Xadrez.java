73873bf24d49e815cc7751cc1ff309a606d8209f
Maksn06 <maksonsigurd@outlook.com>
2019-01-03 18:20:18 -0200

    Começando a implementaçao do tabuleiro e peça

--- src/xadrez/Xadrez.java
+++ src/xadrez/Xadrez.java
@@ -1,15 +1,14 @@
 
 package xadrez;
 
-import Tabuleiro.Posicao;
+import Tabuleiro.Tabuleiro;
 
 
 public class Xadrez {
 
     public static void main(String[] args) {
         
-        Posicao pos = new Posicao(3, 5);
-        System.out.println(pos);
+        Tabuleiro tabuleiro = new Tabuleiro(8, 8);
     }
     
 }
