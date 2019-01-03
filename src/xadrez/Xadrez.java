921853221fe5faa2fadf11b3f5b42ef020c3d416
Maksn06 <maksonsigurd@outlook.com>
2019-01-03 18:06:43 -0200

    Primeira classe: Posição

--- src/xadrez/Xadrez.java
+++ src/xadrez/Xadrez.java
@@ -1,11 +1,15 @@
 
 package xadrez;
 
+import Tabuleiro.Posicao;
+
 
 public class Xadrez {
 
     public static void main(String[] args) {
-        System.out.println("helo");
+        
+        Posicao pos = new Posicao(3, 5);
+        System.out.println(pos);
     }
     
 }
